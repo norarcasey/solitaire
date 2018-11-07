b0f6a1da29f3429c9f32194464f21bc553b9e3db
Justin McKay <1891757+jcmckay@users.noreply.github.com>
2018-11-06 17:13:53 -0800

    added draggable cards

---
+++ src/use-draggable.jsx
@@ -0,0 +1,61 @@
+// The drag and drop functionality is found on stack overflow
+// https://stackoverflow.com/questions/20926551/recommended-way-of-making-react-component-div-draggable
+
+import { useState, useEffect } from 'react';
+
+export default function UseDraggable() {
+  let [isDragging, setIsDragging] = useState(false);
+  let [position, setPosition] = useState({ x: 0, y: 0 });
+  let [relativePosition, setRelativePosition] = useState(null);
+
+  let onMouseDown = function(e) {
+    if (e.button !== 0) return;
+    // var pos = { x: e.target.offsetLeft, y: e.target.offsetTop };
+    setIsDragging(true);
+    setRelativePosition({
+      x: e.pageX - position.x,
+      y: e.pageY - position.y
+    });
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  let onMouseUp = function(e) {
+    setIsDragging(false);
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  let onMouseMove = function(e) {
+    if (!isDragging) return;
+    setPosition({
+      x: e.pageX - relativePosition.x,
+      y: e.pageY - relativePosition.y
+    });
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  useEffect(
+    () => {
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+
+      return () => {
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      };
+    },
+    [isDragging]
+  );
+
+  return [
+    {
+      // position: position.x + position.y === 0 ? 'relative' : 'absolute',
+      position: 'absolute',
+      left: position.x + 'px',
+      top: position.y + 'px'
+    },
+    onMouseDown
+  ];
+}
