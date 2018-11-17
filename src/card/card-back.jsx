b21f4a73faae8f3ee024ec1b64a3f37d7602af4c
Justin McKay <1891757+jcmckay@users.noreply.github.com>
2018-11-17 11:12:26 -0800

    added card flipping

---
+++ src/card/card-back.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+export default function CardBack() {
+  return <div className={styles['card-back']} />;
+}
--- src/card/index.jsx
+++ src/card/index.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useDraggable from '../use-draggable';
+import CardBack from './card-back';
 import styles from './styles.module.css';
 
 export default function Card({ suit, displayValue, value }) {
   let [style, onDragging] = useDraggable();
+  let [isFaceUp, setFaceUp] = useState(true);
 
   let suitUnicodeMap = {
     hearts: '\u2665',
@@ -22,27 +24,37 @@ export default function Card({ suit, displayValue, value }) {
 
   let cardColor = ['diamonds', 'hearts'].includes(suit) ? 'red' : 'black';
 
+  let handleCardFlip = function() {
+    setFaceUp(!isFaceUp);
+  };
+
   return (
-    <div className={styles.card} style={{ color: cardColor, ...style }} onMouseDown={onDragging}>
-      <div className={styles.left}>
-        <div className={styles['display-number']}>{`${displayValue}`}</div>
-      </div>
-      <div className={styles.center}>
-        {value <= 10 ? (
-          Array.from(Array(value), (val, i) => {
-            return (
-              <div key={val + suit + i} className={styles.suit} style={{ width: `${suitIconWidth()}%` }}>
-                {suitUnicodeMap[suit]}
-              </div>
-            );
-          })
-        ) : (
-          <div className={styles['special-card']}>{displayValue}</div>
-        )}
-      </div>
-      <div className={styles.right}>
-        <div className={styles['display-number']}>{`${displayValue}`}</div>
-      </div>
+    <div className={styles.card} style={{ color: cardColor, ...style }} onMouseDown={onDragging} onDoubleClick={handleCardFlip}>
+      {isFaceUp ? (
+        <>
+          <div className={styles.left}>
+            <div className={styles['display-number']}>{`${displayValue}`}</div>
+          </div>
+          <div className={styles.center}>
+            {value <= 10 ? (
+              Array.from(Array(value), (val, i) => {
+                return (
+                  <div key={val + suit + i} className={styles.suit} style={{ width: `${suitIconWidth()}%` }}>
+                    {suitUnicodeMap[suit]}
+                  </div>
+                );
+              })
+            ) : (
+              <div className={styles['special-card']}>{displayValue}</div>
+            )}
+          </div>
+          <div className={styles.right}>
+            <div className={styles['display-number']}>{`${displayValue}`}</div>
+          </div>
+        </>
+      ) : (
+        <CardBack />
+      )}
     </div>
   );
 }
